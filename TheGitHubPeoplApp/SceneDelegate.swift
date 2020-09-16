1cd83da41c4750518cfc48cdbb9b8b2bfd66b800
Richard Price <mrspondooli@gmail.com>
2020-09-16 10:12:00 +0100

    Added test viewcontroller, added assets project setup, also downloaded SF symbols

--- TheGitHubPeoplApp/SceneDelegate.swift
+++ TheGitHubPeoplApp/SceneDelegate.swift
@@ -11,13 +11,19 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    var anotherWindow: UIWindow?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // allows for multiple instances of the same app by using scene delegate
+
+        //Step 1 - new setup for non storyboard apps
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
