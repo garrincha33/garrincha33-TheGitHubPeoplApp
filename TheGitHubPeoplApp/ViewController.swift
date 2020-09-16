8ffcbb406b4f1e03f3eccde5b0c10651d58102ad
Richard Price <mrspondooli@gmail.com>
2020-09-16 09:10:09 +0100

    Initial Commit

---
+++ TheGitHubPeoplApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheGitHubPeoplApp
+//
+//  Created by Richard Price on 16/09/2020.
+//  Copyright © 2020 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
